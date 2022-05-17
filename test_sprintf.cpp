1c0c8f6b87ca449e137e78ae77c44948e5a901ea
Jiac_W <643510549@qq.com>
2022-05-17 22:43:52 +0800

    update

---
+++ test_sprintf.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+typedef unsigned long size_t;
+
+void func(size_t& length)
+{
+	length = 100;
+}
+
+int main()
+{
+	char ipString[40] = {};
+	unsigned char value = 255;
+	sprintf(ipString, "%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x:%x%x", value,value,value,value,value,
+			value,value,value,value,value,
+			value,value,value,value,value,value);
+	// cout << ipString << endl;
+
+
+	unsigned char mcc[10] = {};
+	unsigned char plmmString = 255;
+	sprintf((char*)mcc, "%u%u%u", plmmString, plmmString, plmmString);
+	cout << mcc << endl;
+
+
+	unsigned int  a = 10;
+	unsigned int  ptr = a;
+	func((size_t&)ptr);
+	cout << ptr << endl;
+	
+	char arr[100] = {0};
+	arr[10] = '\0';
+	cout << "sizeof(char*) = " << sizeof(arr) << endl;
+	cout << "strlen(char*) = " << strlen(arr) << endl;
+
+	cout << "This is test for rebase." << endl;
+
+	cout << "Today is 7/20." << endl;
+}
